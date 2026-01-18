98d6386af8652989cf4f9c55f70b97bd594d9d6b
rehanjaswal <rehanxjaswal@gmail.com>
2026-01-18 21:16:30 +0530

    basic typecasting

--- 03_typecasting/01_basic_typecasting.py
+++ 03_typecasting/01_basic_typecasting.py
@@ -1,25 +1,11 @@
-x = 7 
-print(x)
-print(type(x))
-
-y = 17 
-print(y)
-print(type(y))
+age_str = "19"
+print(type(age_str))
+age_num = int(age_str)
+print(age_num)
+print(type(age_num))
 
 euler_constant = 2.718
 print(euler_constant)
 print(type(euler_constant))
-
-z = "20"
-print(z)
-
-print(type(z))
-p = int(z)
-print(p)
-print(type(p))
-
-
-pi = 3.14
-pi_int = int(pi)
-print(pi_int)
-print(type(pi_int))
+euler_constant_int = int(euler_constant)
+print(euler_constant_int)
