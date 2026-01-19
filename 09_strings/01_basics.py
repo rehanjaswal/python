3704438707fd468cc575ad978db64d5362dc236e
rehanjaswal <rehanxjaswal@gmail.com>
2026-01-19 09:22:13 +0530

    strings basics

--- 09_strings/01_basics.py
+++ 09_strings/01_basics.py
@@ -1,10 +1,7 @@
-name1 = "rehan"
-name2 = 'rehanjaswal'
-name3 = '''ashu'''      #multi line string
-print(name1)
-print(name2)
-print(name3)
+name = "rehan"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
 
-college_and_goals = '''NIT Jalandhar undergrad
-wanna become the best version of myself'''           # multi line string  
-print(college_and_goals)#
--- 09_strings/02_indexing.py
+++ 09_strings/02_indexing.py
@@ -1,9 +1,8 @@
 # name = "R  E  H  A  N"
-# #       1  2  3  4  5
+# #       0  1  2  3  4
 # #      -5 -4 -3 -2 -1
 
 name = "REHAN"
-print(name[1:3:1])
-print(name[:4])
-print(name[2:])
-print(name[0:4:2])
+print(name[1:4])   # 1 to 4-1 -> 1 to 3   EHA
+print(name[:4])    # replace the empty space with 0 REHA
+print(name[1:])   # replace the empty space with length - 1    EHAN
