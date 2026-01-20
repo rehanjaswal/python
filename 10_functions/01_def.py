919e00c69ecdb73604e1dbd3e5be0145de9b3e49
rehanjaswal <rehanxjaswal@gmail.com>
2026-01-20 19:55:42 +0530

    def, lambda

--- 10_functions/01_def.py
+++ 10_functions/01_def.py
@@ -1,9 +1,27 @@
-def average (a,b,c):
-    avg = (a+b+c)/2.0
-    return avg
+# def average(a, b, c):
+#     d = (a + b + c) / 3.0
+#     return d
 
-avg1 = average(1,3,5)
-avg2 = average(2,4,7)
+# avg1 = average(1, 2, 3)
+# avg2 = average(4, 5, 6)
 
-print(avg1)
-print(avg2)
+# print(avg1)
+# print(avg2)
+
+
+# def add(a, b):
+#     return a + b
+
+# # c= add(2,3)
+# # print(c)
+
+# print(add(2,3))   
+
+# def subtract(a,b):
+#     return a - b
+
+# print("a - b =",subtract(b = 7, a = 10))
+
+def student(age, name):
+    return f"name of student is {name} and age of student is {age}."
+print(student(19, "Rehan"))
--- 10_functions/02_lambda.py
+++ 10_functions/02_lambda.py
@@ -1,3 +1,12 @@
-cube = lambda x: x*x*x
-print("cube of 3 is", (cube(3)))
+cube = (lambda x: x*x*x)
+sum = lambda a,b: a + b
 
+# print(cube(3))
+# print(sum(2,3))
+
+x = int(input("enter the number whose cube you wanna find: "))
+a = int(input("for addition, enter first number: "))
+b = int(input("for addition, enter second number: "))
+
+print("cube of",x,"is",cube(3))
+print("sum of",a,"and",b,"is",sum(a, b))  
