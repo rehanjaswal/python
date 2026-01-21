aa885bec4b2cf6f870943f9da7d8560a3667112d
rehanjaswal <rehanxjaswal@gmail.com>
2026-01-21 23:43:05 +0530

    had fun solving this

--- problem_solving/21_jan_26/06_vowel_consonant_counter.py
+++ problem_solving/21_jan_26/06_vowel_consonant_counter.py
@@ -1,5 +1,11 @@
-s = input("enter a string").lower()
+s = input("enter a string: ").lower()
 vowel = 0
 consonant = 0
 for ch in s:
     if ch in "aeiou":
+        vowel+=1
+    elif ch.isalpha():
+        consonant+=1
+
+print(f"vowels = {vowel}")
+print(f"consonants = {consonant}")
