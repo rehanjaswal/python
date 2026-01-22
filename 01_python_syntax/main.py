85b0211f3b6f309d598cbe0bee40d79023eb8efc
rehanjaswal <rehanxjaswal@gmail.com>
2026-01-22 18:25:54 +0530

    fixing my stuff

--- 01_python_syntax/main.py
+++ 01_python_syntax/main.py
@@ -1,6 +1,6 @@
 print("Hello Rehan")
 print("How are you?")
-print("I'm good Akshi, what about you?")
+print("I'm good bro what about ya?")
 
 # to run this, either press the run button or write python file name.py in the terminal
 
---
+++ college/Q3.py
@@ -0,0 +1,4 @@
+# let ages be x and y and x > y
+
+x - y = 44
+x * y = 1280
