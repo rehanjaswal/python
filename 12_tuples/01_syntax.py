4139bc278f743c4f5aad7ff673fb0455b5acd468
rehanjaswal <rehanxjaswal@gmail.com>
2026-03-14 12:48:10 +0530

    tuples

---
+++ 12_tuples/01_syntax.py
@@ -0,0 +1,11 @@
+tu = (1, 2, 3, 4, 5)
+print(tu)
+
+tuple = (67, 69, 106, 666)
+
+a, b, c, d = tuple
+
+print(a, b, c, d)
+
+#  tuples are immutable, faster than lists, used as dictionary keys cuz they're hashable and tuples prevent from unintended modifications/changes
+
